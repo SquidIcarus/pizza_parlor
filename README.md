f273f3206a0742de98062d1bf733e3f833b75e6a
Landon Isaiah <landonisaiah@gmail.com>
2023-12-09 16:31:23 -0800

    Add MyPie prototype to add toppings with TDD in readme.

--- README.md
+++ README.md
@@ -12,10 +12,18 @@ TEST: "Creates an object of an empty array to hold the total amount of pizzas.";
 CODE: let pizzaOrders = new TotalPizzas();
 EXPECT: _TotalPizzas_ {pizzas: {…}};
 
-DESCRIPTION: MyPie(size, dough, sauce, toppings, cheese);
+DESCRIPTION: MyPie(size, dough, sauce, topping, cheese);
 
 TEST: "Creates an object with a pizza build of choice size, dough, sauce, toppings and cheese."
 CODE: let pizzaOne = new MyPie("12", "Chicago", "Tomato", "Pepperoni", "Mozzarella");
 EXPECT: _MyPie_ {size: '12', dough: 'Chicago', sauce: 'Tomato', toppings: 'Pepperoni', cheese: 'Mozzarella'}
+
+DESCRIPTION: MyPie.prototype.addTopping(topping)
+
+TEST: "Adds toppings to empty array in MyPie object properties."
+CODE: pizzaOne.addTopping(["pepperoni", "mushroom"]);
+EXPECT: toppings: Array(1)
+        0: (2) ['pepperoni', 'mushroom']
+        length:1
 
 ```
--- js/scripts.js
+++ js/scripts.js
@@ -11,31 +11,19 @@ function TotalPizzas() {
     this.pizzas = {};
 }
 
-function MyPie(size, dough, sauce, toppings, cheese) {
+function MyPie(size, dough, sauce, [], cheese) {
     this.size = size;
     this.dough = dough;
     this.sauce = sauce;
-    this.toppings = [toppings];
+    this.toppings = [];
     this.cheese = cheese;
 }
 
+MyPie.prototype.addTopping = function (topping) {
+    this.toppings.push(topping);
+};
+
 // User Interface Logic
-function handleForm(event) {
-    event.preventDefault();
-    const userSelections = document.querySelectorAll("input[name=topping]:checked");
-    const userSelectionsArray = Array.from(userSelections);
 
-    const resultsHeading = document.createElement("h2");
-    resultsHeading.append("Your pizza order:");
-    document.body.append(resultsHeading);
 
-    userSelectionsArray.forEach(function (element) {
-        const paragraph = document.createElement("p");
-        paragraph.append(element.value);
-        document.body.append(paragraph);
-    });
-}
 
-window.addEventListener("load", function () {
-    this.document.querySelector("form#pizzaOrder").addEventListener("submit", handleForm);
-});
